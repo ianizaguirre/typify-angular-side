c5d989a5070d99298ec7840a06a46ca92b16ed76
[ianizaguirre] <ian@izaguir.re>
2017-12-12 09:26:14 -0500

    Add: app-routing-module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	// { path: '',           component:  HomePageComponent },
+	// { path: 'phones',     component:  PhoneListComponent },
+	// { path: 'phones/:id', component:  PhoneDetailsComponent },
+	// { path: 'join',       component:  JoinPageComponent },
+
+	{ path: '**',         component:  NotFoundComponent }
+];
+
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
