faf60ae1e16a197368e3ce2fd8cc10153f6fd413
[ianizaguirre] <ian@izaguir.re>
2017-12-14 13:07:44 -0500

    test: going to remove styles.css to organize css better under assets folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
+import { HomePageComponent } from './home-page/home-page.component';
+import { ArticleComponent } from './article/article.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+
+
+
 const routes: Routes = [
-	// { path: '',           component:  HomePageComponent },
+	{ path: '',           component:  HomePageComponent },
+	{ path: 'article',           component:  ArticleComponent },
 	// { path: 'phones',     component:  PhoneListComponent },
 	// { path: 'phones/:id', component:  PhoneDetailsComponent },
 	// { path: 'join',       component:  JoinPageComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ArticleComponent } from './article/article.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    HomePageComponent
+    HomePageComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
