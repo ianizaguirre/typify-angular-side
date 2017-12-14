9ea110ef8d4787da80abf945ef887a2607fb577c
[ianizaguirre] <ian@izaguir.re>
2017-12-14 09:52:33 -0500

    inital commit to github connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
